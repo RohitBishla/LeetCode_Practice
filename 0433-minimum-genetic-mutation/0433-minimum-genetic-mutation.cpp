a1d6316ff49911250bb1731919602829fbf3b933
Rohit Bishla <77221475+RohitBishla@users.noreply.github.com>
2022-11-02 19:39:08 +0530

    Time: 4 ms (29.93%), Space: 6.9 MB (7.33%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -39,8 +39,39 @@ public:
         for(int i = 0; i < bank.size(); i++){
             map[bank[i]] = 1;
         }
-        int ans = helper(start, end, map);
-        if(ans == 9) return -1;
-        return ans;
+        queue<string> qu;
+        qu.push(start);
+        qu.push("");
+        int count = 0;
+        while(!qu.empty()){
+            string s = qu.front();
+            qu.pop();
+            int a = 0;
+            if(s == ""){
+                count++;
+                if(!qu.empty())
+                    qu.push("");
+            }
+            for(int i = 0; i < 8; i++){
+                if(s[i] != end[i]) a++;
+            }
+            if(a == 0) return count;
+            for(int i = 0; i < 8; i++){
+                int last = s[i];
+                for(int j = 0; j < 4; j++){
+                    if(last == c[j]) continue;
+                    s[i] = c[j];
+                    if(map[s] == 1){
+                        qu.push(s);
+                        map[s] = 0;
+                    }
+                    s[i] = last;
+                }
+            }
+        }
+        return -1;
+        // int ans = helper(start, end, map);
+        // if(ans == 9) return -1;
+        // return ans;
     }
 };
