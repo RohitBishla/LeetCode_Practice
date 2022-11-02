811897db2dcc354c53c252f3022fd263367eddaf
Rohit Bishla <77221475+RohitBishla@users.noreply.github.com>
2022-11-02 19:39:35 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (15.87%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,38 +1,38 @@
 class Solution {
     vector<char> c = {'A', 'C', 'G', 'T'};
-    int helper(string start, string& end, unordered_map<string, int>& map){
-        // if(i == 0){
-        //     for(int i = 0; i < 8; i++){
-        //         if(start[i] != end[i]){
-        //            return 8; 
-        //         }
-        //     }
-        //     return 0;
-        // }
-        int count = 0;
-        for(int i = 0; i < 8; i++){
-            if(start[i] != end[i]) count++;
-        }
-        if(count == 0) return 0;
-        // int a = helper(start, end, map);
-        int ans = 9;
-        for(int i = 0; i < 8; i++){
-            char last = start[i];
-            // if(start[i] != end[i]){
-                for(int j = 0; j < 4; j++){
-                    if(c[j] == last) continue;
-                    start[i] = c[j];
-                    if(map[start] == 1){
-                        map[start] = 0;
-                        ans = min(ans, 1 + helper(start, end, map));
-                        map[start] = 1;
-                    }
-                    start[i] = last;
-                }
-            // }
-        }
-        return ans;
-    }
+    // int helper(string start, string& end, unordered_map<string, int>& map){
+    //     // if(i == 0){
+    //     //     for(int i = 0; i < 8; i++){
+    //     //         if(start[i] != end[i]){
+    //     //            return 8; 
+    //     //         }
+    //     //     }
+    //     //     return 0;
+    //     // }
+    //     int count = 0;
+    //     for(int i = 0; i < 8; i++){
+    //         if(start[i] != end[i]) count++;
+    //     }
+    //     if(count == 0) return 0;
+    //     // int a = helper(start, end, map);
+    //     int ans = 9;
+    //     for(int i = 0; i < 8; i++){
+    //         char last = start[i];
+    //         // if(start[i] != end[i]){
+    //             for(int j = 0; j < 4; j++){
+    //                 if(c[j] == last) continue;
+    //                 start[i] = c[j];
+    //                 if(map[start] == 1){
+    //                     map[start] = 0;
+    //                     ans = min(ans, 1 + helper(start, end, map));
+    //                     map[start] = 1;
+    //                 }
+    //                 start[i] = last;
+    //             }
+    //         // }
+    //     }
+    //     return ans;
+    // }
 public:
     int minMutation(string start, string end, vector<string>& bank) {
         unordered_map<string, int> map;
