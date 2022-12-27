023c7feb2267ef0e952a8d47b4b0d1b16daba985
Rohit Bishla <77221475+RohitBishla@users.noreply.github.com>
2022-12-27 19:08:20 +0530

    Added solution - LeetHub

---
+++ Container With Most Water - GFG/container-with-most-water.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+// int maxArea(int A[], int len);
+
+// } Driver Code Ends
+//User function template for C++
+
+long long maxArea(long long A[], int len)
+{
+    // Your code goes here
+    long long ans = 0;
+    int i = 0;
+    int j = len - 1;
+    while(i < j){
+        if(A[i] > A[j]){
+            ans = max(ans, (j - i)*A[j]);
+            j--;
+        }
+        else{
+            ans = max(ans, (j - i)*A[i]);
+            i++;
+        }
+    }
+    return ans;
+}
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+    {
+        int n;
+        cin>>n;
+        long long arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cout<<maxArea(arr,n)<<endl;
+    }
+return 0;
+}
+
+// } Driver Code Ends
