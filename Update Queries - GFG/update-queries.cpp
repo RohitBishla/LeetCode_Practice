9c3de49be61354ff42a9cbdf8b007156c179f266
Rohit Bishla <77221475+RohitBishla@users.noreply.github.com>
2023-03-01 22:24:49 +0530

    Added solution - LeetHub

--- Update Queries - GFG/update-queries.cpp
+++ Update Queries - GFG/update-queries.cpp
@@ -5,26 +5,13 @@
 using namespace std;
 
 // } Driver Code Ends
-//User function Template for C++
 
-//User function Template for C++
-
-
-// N*N = 
-// N = 5
-// 1 5 2 
-// 1 5  3
-// 1 5 2
-// 1 5 5
-// 1 5 0
-// 1 5 
+ 
 
 class Solution{
     vector<int> bin(int x){
         vector<int> arr(32, 0);
-        // 4 => 100
-        // 3 =>  11
-        // 10
+
         int i = 0;
         while(x > 0){
             if(x%2){
@@ -35,13 +22,7 @@ class Solution{
         }
         return arr;
     }
-    // 0100000000 => 2 pow(2, 1);
-    // 0000000010
-    // 2^0 + 2^1
-    // 1<<2 => 2^2
-    // 1<<5 => 2^5\
-    11
-    // 2, 3, 4, 5 => 111^10 => 101
+
     int num(vector<int>& arr){
         int ans = 0;
         for(int i = 0; i < 32; i++){
