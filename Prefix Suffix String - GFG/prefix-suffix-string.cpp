ca4b0ad1b3e219e6d39467ccd8ea2b0bbcc4a6ec
Rohit Bishla <77221475+RohitBishla@users.noreply.github.com>
2023-04-21 10:09:28 +0530

    Added solution - LeetHub

---
+++ Prefix Suffix String - GFG/prefix-suffix-string.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int prefixSuffixString(vector<string> &s1,vector<string> s2){
+        unordered_map<string, int> map;
+        for(int i = 0; i < s1.size(); i++){
+            for(int j = 0; j < s1[i].size(); j++){
+                map[s1[i].substr(0, j)] = 1;
+            }
+            for(int j = s1[i].size() - 1; j > 0; j--){
+                map[s1[i].substr(j)] = 1;
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < s2.size(); i++){
+            if(map.count(s2[i])) ans++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        string str1,str2,newline;
+        if(i==0)
+        getline(cin,newline);
+        getline(cin,str1);
+        getline(cin,str2);
+        // getline(cin,newline);
+        // cout<<str1<<endl<<str2<<endl;
+        vector<string> s1,s2;
+        stringstream ss1(str1);
+        stringstream ss2(str2);
+        string str;
+        while(ss1>>str){
+            s1.push_back(str);
+        }
+        while(ss2>>str){
+            s2.push_back(str);
+        }
+        Solution ob;
+        cout<<ob.prefixSuffixString(s1, s2)<<endl;
+    }
+}
+// } Driver Code Ends
