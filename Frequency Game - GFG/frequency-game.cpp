c8f3cc67ed9a3de7fca9c8ffcb5924d307a7489e
Rohit Bishla <77221475+RohitBishla@users.noreply.github.com>
2023-05-31 11:29:44 +0530

    Added solution - LeetHub

--- Frequency Game - GFG/frequency-game.cpp
+++ Frequency Game - GFG/frequency-game.cpp
@@ -11,21 +11,18 @@ using namespace std;
 class Solution{
 public:
     int LargButMinFreq(int arr[], int n) {
-        sort(arr, arr + n);
+        unordered_map<int, int> map;
+        for(int i = 0; i < n; i++) map[arr[i]]++;
+        int freq = n;
         int ans = arr[0];
-        int len = n;
-        int i = 0;
-        while(i < n){
-            int count = 1;
-            while(i < n - 1 && arr[i] == arr[i + 1]){
-                count++;
-                i++;
+        for(auto it: map){
+            if(it.second == freq){
+                if(ans < it.first) ans = it.first;
             }
-            if(count <= len){
-                len = count;
-                ans = arr[i];
+            else if(it.second < freq){
+                ans = it.first;
+                freq = it.second;
             }
-            i++;
         }
         return ans;
     }
