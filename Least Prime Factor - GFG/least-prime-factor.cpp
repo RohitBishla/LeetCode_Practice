778cbd6c369f7ac166ada639b1f26ecad7c2ca83
Rohit Bishla <77221475+RohitBishla@users.noreply.github.com>
2023-06-07 11:35:54 +0530

    Added solution - LeetHub

---
+++ Least Prime Factor - GFG/least-prime-factor.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> leastPrimeFactor(int n) {
+        vector<int> ans(n + 1);
+        ans[1] = 1;
+        for(int i = 2; i < n + 1; i++){
+            if(ans[i] == 0){
+                for(int j = i; j < n + 1; j += i){
+                    if(ans[j] == 0) ans[j] = i;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        vector<int>ans = ob.leastPrimeFactor(n);
+        for(int i=1;i<=n;i++)cout<<ans[i]<<" ";
+        cout<<endl;  
+    }
+    return 0;
+}
+
+// } Driver Code Ends
