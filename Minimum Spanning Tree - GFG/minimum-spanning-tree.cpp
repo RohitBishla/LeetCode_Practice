efcb04f49b1643965dce72b2ce4ffebe70b9847e
Rohit Bishla <77221475+RohitBishla@users.noreply.github.com>
2023-06-22 14:34:13 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,24 +3,56 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class dsu{
+    vector<int> parent, size;
+public:
+    dsu(int n){
+        parent.resize(n);
+        size.resize(n);
+        for(int i = 0; i < n; i++) parent[i] = i;
+    }
+
+    int findUParent(int node){
+        if(parent[node] == node) return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+
+    int unionBySize(int u, int v){
+        int pu = findUParent(u);
+        int pv = findUParent(v);
+        if(size[pu] >= size[pv]){
+            size[pu] += size[pv];
+            parent[pv] = pu;
+        }
+        else if(size[pu] < size[pv]){
+            size[pv] += size[pu];
+            parent[pu] = pv;
+        }
+    }
+};
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
-    {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> visited(V, 0);
-        pq.push({0, 0});
+    int spanningTree(int V, vector<vector<int>> adj[]){
+        vector<vector<int>> edge;
+        for(int i = 0; i < V; i++){
+            for(auto it: adj[i]){
+                edge.push_back({it[1], i, it[0]});
+            }
+        }
+        sort(edge.begin(), edge.end());
+        dsu d(V);
         int summ = 0;
-        while(!pq.empty()){
-            pair<int, int> p = pq.top();
-            pq.pop();
-            if(visited[p.second]) continue;
-            visited[p.second] = 1;
-            summ += p.first;
-            for(auto it: adj[p.second]){
-                pq.push({it[1], it[0]});
+        for(int i = 0; i < edge.size(); i++){
+            int pu = edge[i][1];
+            int pv = edge[i][2];
+            // cout << pu << ", " << pv << "--" << d.findUParent(pu) << ", " << d.findUParent(pv) << endl;
+            if(d.findUParent(pu) != d.findUParent(pv)){
+                summ += edge[i][0];
+                d.unionBySize(pu, pv);
             }
         }
         return summ;
